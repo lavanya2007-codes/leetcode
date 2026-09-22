4d7120966155cea9ed6742539fa12ed5cd790d38
lavanya2007-codes <lavanya.singh112@gmail.com>
2026-09-22 12:02:03 +0530

    Time: 3 ms (55.43%), Space: 9.2 MB (85.33%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,14 @@
+int reverse(int x){
+    int remainder;
+    long reverse=0;
+    while(x!=0){
+    remainder=x%10;
+    reverse=reverse*10+remainder;
+    x=x/10;
+    }
+    if(reverse>INT_MAX ||reverse<INT_MIN){
+        return 0;
+    }
+    return reverse;
+
+}
